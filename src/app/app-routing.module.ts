e611a97c9b2be0b3bf42a300d0260f4e9a03778f
Rubén Valls <r.vallsaparici@gmail.com>
2023-02-02 00:40:05 +0100

    adding routing between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SecondPageComponent} from './second-page/second-page.component'
+import {ThirdPageComponent} from './third-page/third-page.component'
+import {FirstPageComponent} from './first-page/first-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstPageComponent},
+  {path: 'secondPage-component', component: SecondPageComponent},
+  {path: 'thirdPage-component', component: ThirdPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
